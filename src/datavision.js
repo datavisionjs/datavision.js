3f24cf03d076569888eeeaa80b7cba6c63fc9688
Arthur Arnue Kennedy <arthurakennedy@gmail.com>
2023-10-06 15:22:33 -0500

    Added all folders with respective files

---
+++ src/datavision.js
@@ -0,0 +1,14 @@
+
+
+function DataVision(targetId){
+    this.target = document.getElementById(targetId);
+    this.canvas = document.createElement("canvas");
+}
+
+DataVision.prototype.plot = function (type, data, layout){
+
+}
+
+DataVision.prototype.process = function () {
+
+}
